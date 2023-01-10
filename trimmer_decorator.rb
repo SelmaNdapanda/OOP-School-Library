0d86c5ff5e6207fd5c87f3d9d7eba257b3e038ba
Selma <selmandapanda@yahoo.com>
2023-01-10 16:37:53 +0200

    Fix linters errors

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,16 +1,18 @@
 require './decorator'
 
 class TrimmerDecorator < Decorator
-    def correct_name
-        @nameable.correct_name.length > 10?
-        @nameable.correct_name[0..9] :
-        @nameable.correct_name
+  def correct_name
+    if @nameable.correct_name.length > 10
+      @nameable.correct_name[0..9]
+    else
+      @nameable.correct_name
     end
+  end
 end
 
 person = Person.new(22, 'maximilianus')
-  person.correct_name
-  capitalizedPerson = CapitalizeDecorator.new(person)
-  capitalizedPerson.correct_name
-  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-  capitalizedTrimmedPerson.correct_name
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+capitalized_trimmed_person.correct_name
