ded038dcaf0194cec1a016c76ccc32ff8a39677d
Selma <selmandapanda@yahoo.com>
2023-01-10 15:17:07 +0200

    Turn Person class to Nameable

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
@@ -15,5 +17,9 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
