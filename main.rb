20326bd69516d39836233b1261c3ff70fa0d84a2
Selma <selmandapanda@yahoo.com>
2023-01-16 20:33:10 +0200

    Display the options list

--- main.rb
+++ main.rb
@@ -1,32 +1,48 @@
 require_relative 'app'
 
-def main
-  app = App.new
-  app.start
+def display_list
+  puts ''
+  puts 'Welcome to School Library App!'
+  puts ''
+
+  # option = nil
+  puts "
+  Please choose an option by entering a number:
+  1 - List all books
+  2 - List all people
+  3 - Create a person
+  4 - Create a book
+  5 - Create a rental
+  6 - List all rentals for a given person id
+  7 - Exit"
 end
 
-def option(input)
-  case input
-  when '1'
-    list_books
-  when '2'
-    list_people
-  when '3'
-    create_person
-  when '4'
-    create_book
-  when '5'
-    create_rental
-  when '6'
-    list_rentals_for_person_id
-  else
-    exit_app(app)
+def options(app)
+  loop do
+    display_list
+    option = gets.chomp.to_i
+    case option
+    when '1'
+      list_books
+    when '2'
+      list_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals_for_person_id
+    else
+      break
+    end
   end
 end
 
-def exit_app(_app)
-  puts 'Thank you for using this app!'
-  exit
+def main
+  app = App.new
+  options(app)
 end
 
 main
